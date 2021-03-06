d65e12973a879eef6c4b5bacca16db346a01120c
Sara Jirholm <sara.jirholm@hotmail.com>
2021-03-06 18:24:26 +0100

    Refactor scrapers

--- scrapers.js
+++ scrapers.js
@@ -1,38 +1,59 @@
 const puppeteer = require("puppeteer"); // Can I use es6 modules?
 
 // https://www.youtube.com/watch?v=4q9CNtwdawA&ab_channel=FabianGrohs
-const fabianWay = async (url) => {
+const fetchProductsFromGronvaxtriket = async (url) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  page.on("console", (msg) => {
-    for (let i = 0; i < msg.args().length; ++i)
-      console.log(`${i}: ${msg.args()[i]}`);
-  });
+  fixConsoleLog(page);
+
   await page.goto(url);
 
-  const getProducts = async () =>
-    await page.$$eval(".product", (products) =>
-      products.map((product) => ({
-        name: product.querySelector("h2").innerText,
-        price: product.querySelector(".price").innerText,
-      }))
-    );
-
-  const nextPageButton = () => page.$(".page-numbers .next");
-
-  let allProducts = [];
-  while (await nextPageButton()) {
-    const products = await getProducts();
-    allProducts = [...allProducts, ...products];
-    const button = await nextPageButton();
-
-    button.click();
-    await page.waitForNavigation();
-  }
-
+  const allProducts = await findAndTransformAllProducts(page);
   console.log("allProducts", allProducts);
 
   await browser.close();
 };
 
-fabianWay("https://gronvaxtriket.se/product-category/alla-vaxter/");
+function fixConsoleLog(page) {
+  page.on("console", (msg) => {
+    for (let i = 0; i < msg.args().length; ++i)
+      console.log(`${i}: ${msg.args()[i]}`);
+  });
+}
+
+async function findAndTransformAllProducts(page) {
+  let allProducts = [];
+
+  while (await findNextPageButton(page)) {
+    const products = await getProducts(page);
+    allProducts = [...allProducts, ...products];
+    await pressNextPage(page);
+  }
+
+  return allProducts;
+}
+
+async function findNextPageButton(page) {
+  return await page.$(".page-numbers .next");
+}
+
+const getProducts = async (page) => {
+  return await page.$$eval(".product", transformProducts);
+};
+
+const transformProducts = (products) =>
+  products.map((product) => ({
+    name: product.querySelector("h2").innerText,
+    price: product.querySelector(".price").innerText,
+  }));
+
+async function pressNextPage(page) {
+  const button = await findNextPageButton(page);
+
+  button.click();
+  await page.waitForNavigation();
+}
+
+fetchProductsFromGronvaxtriket(
+  "https://gronvaxtriket.se/product-category/alla-vaxter/"
+);
